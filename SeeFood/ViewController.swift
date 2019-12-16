4e771eed5033fb93a7cf5f75d73c513742429300
Mishka1012 <mikhailamshei@grodnoapps.com>
2019-12-15 22:17:40 -0600

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Mikhail Amshei on 12/15/19.
+//  Copyright © 2019 Grodno Apps LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
